0ae3d30c947f55b9306ec385efe91220de60d072
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2025-10-15 18:14:17 +0200

    events

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ document.body.appendChild(blackBorderDiv);
 
 blackBorderDiv.appendChild(divHeader);
 blackBorderDiv.appendChild(divP);
+
+// Events
+
+const button = document.createElement("button");
+const alertButton = document.createElement("button");
+button.innerText = "Click me";
+alertButton.innerText = "Alert Function Button";
+
+button.addEventListener("click", () => {
+  alert("Hello World");
+});
+
+document.body.appendChild(button);
+document.body.appendChild(alertButton);
+
+function alertFunction() {
+  alert("Yay! You did it");
+}
+
+alertButton.addEventListener("click", alertFunction);
